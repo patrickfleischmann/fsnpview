d789fcfc5a04f44f7620daf66e181ce3f981180c
patrickfleischmann <patrickfleischmann@users.noreply.github.com>
2025-09-13 21:33:46 +0200

    Add NetworkCascade unit test

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-# This script builds the parser tests.
+# This script builds the parser and network cascade tests.
 set -e
+
 g++ -std=c++17 -I/usr/include/eigen3 -I. tests/parser_touchstone_tests.cpp parser_touchstone.cpp -o parser_touchstone_tests
+
+/usr/lib/qt6/libexec/moc network.h -o /tmp/moc_network.cpp
+/usr/lib/qt6/libexec/moc networkfile.h -o /tmp/moc_networkfile.cpp
+/usr/lib/qt6/libexec/moc networkcascade.h -o /tmp/moc_networkcascade.cpp
+
+g++ -std=c++17 -I/usr/include/eigen3 -I. tests/networkcascade_tests.cpp parser_touchstone.cpp network.cpp networkfile.cpp networkcascade.cpp /tmp/moc_network.cpp /tmp/moc_networkfile.cpp /tmp/moc_networkcascade.cpp $(pkg-config --cflags --libs Qt6Core Qt6Gui) -o networkcascade_tests
