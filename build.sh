ee2314fecececeaa950d92293abb465c70ed50c9
patrickfleischmann <patrickfleischmann@users.noreply.github.com>
2025-09-13 21:42:24 +0200

    Add offscreen GUI plot regression test

--- build.sh
+++ build.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-# This script builds the parser tests.
+# This script builds the tests.
 set -e
+
+MOC=/usr/lib/qt6/libexec/moc
+MOC_INCLUDES="-I/usr/include/x86_64-linux-gnu/qt6 -I/usr/include/x86_64-linux-gnu/qt6/QtCore -I/usr/include/x86_64-linux-gnu/qt6/QtWidgets -I/usr/include/x86_64-linux-gnu/qt6/QtGui -I/usr/include/x86_64-linux-gnu/qt6/QtPrintSupport"
+
 g++ -std=c++17 -I/usr/include/eigen3 -I. tests/parser_touchstone_tests.cpp parser_touchstone.cpp -o parser_touchstone_tests
+
+# Generate moc files for Qt classes
+$MOC $MOC_INCLUDES plotmanager.h -o moc_plotmanager.cpp
+$MOC $MOC_INCLUDES network.h -o moc_network.cpp
+$MOC $MOC_INCLUDES networkfile.h -o moc_networkfile.cpp
+$MOC $MOC_INCLUDES networklumped.h -o moc_networklumped.cpp
+$MOC $MOC_INCLUDES networkcascade.h -o moc_networkcascade.cpp
+$MOC $MOC_INCLUDES qcustomplot.h -o moc_qcustomplot.cpp
+
+# Build GUI plot test
+g++ -std=c++17 -I/usr/include/eigen3 -I. \
+    tests/gui_plot_tests.cpp plotmanager.cpp network.cpp networkfile.cpp \
+    networklumped.cpp networkcascade.cpp parser_touchstone.cpp qcustomplot.cpp \
+    moc_plotmanager.cpp moc_network.cpp moc_networkfile.cpp moc_networklumped.cpp \
+    moc_networkcascade.cpp moc_qcustomplot.cpp \
+    -o gui_plot_tests $(pkg-config --cflags --libs Qt6Widgets Qt6Gui Qt6Core Qt6PrintSupport)
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Run parser touchstone tests.
+# Run parser and GUI plot tests.
 set -e
 ./parser_touchstone_tests
+QT_QPA_PLATFORM=offscreen ./gui_plot_tests
