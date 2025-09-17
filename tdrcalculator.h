65cf971ee602729fc83554bfc345b28e6bf35523
patrickfleischmann <patrickfleischmann@users.noreply.github.com>
2025-09-17 23:49:09 +0200

    Fix TDR parameters default initialization

--- tdrcalculator.h
+++ tdrcalculator.h
@@ -10,9 +10,18 @@ class TDRCalculator
 public:
     struct Parameters
     {
-        double referenceImpedance = 50.0;
-        double effectivePermittivity = 2.9;
-        double speedOfLight = 299792458.0;
+        constexpr Parameters(double referenceImpedance = 50.0,
+                             double effectivePermittivity = 2.9,
+                             double speedOfLight = 299792458.0)
+            : referenceImpedance(referenceImpedance),
+              effectivePermittivity(effectivePermittivity),
+              speedOfLight(speedOfLight)
+        {
+        }
+
+        double referenceImpedance;
+        double effectivePermittivity;
+        double speedOfLight;
     };
 
     struct Result
