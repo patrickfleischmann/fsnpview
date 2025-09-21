eb298fe5a01be76a940aa995a8bfbe83a6a9cdfe
patrickfleischmann <patrickfleischmann@users.noreply.github.com>
2025-09-21 21:33:18 +0200

    Update setup script to install Qt6 packages

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script installs the dependencies for the fsnpview project on Debian-based systems.
 set -e
-sudo apt-get update && sudo apt-get install -y build-essential qt5-qmake qtbase5-dev libeigen3-dev
+sudo apt-get update && sudo apt-get install -y build-essential qt6-base-dev qt6-base-dev-tools libeigen3-dev
